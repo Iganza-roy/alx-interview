3f88e4e4f9d7271d162ce8104f8efc2321e7c9a8
Iganza-roy <iganzaroy55@gmail.com>
2024-08-30 00:04:14 +0300

    Update task 1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
-import sys
 
-def is_safe(board, row, col, N):
+"""
+A program that solves the N queens problem
+"""
+
+def is_safe(board, row, col):
+    """Checks if placing a queen at (row, col) is safe."""
     for i in range(row):
         if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
+           abs(board[i] - i) == abs(col - row):
             return False
     return True
 
 def solve_nqueens(N):
+    """Solves the N-Queens problem."""
     def backtrack(row, board):
         if row == N:
-            print([[i, board[i]] for i in range(N)])
+            print(board)
         else:
             for col in range(N):
-                if is_safe(board, row, col, N):
+                if is_safe(board, row, col):
                     board[row] = col
                     backtrack(row + 1, board)
+                    board[row] = -1
 
     board = [-1] * N
     backtrack(0, board)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    
+    N = int(input("Enter the board size (N): "))
     if N < 4:
         print("N must be at least 4")
-        sys.exit(1)
-    
-    solve_nqueens(N)
+    else:
+        solve_nqueens(N)
