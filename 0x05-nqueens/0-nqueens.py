760ddbde11106b8ddb6c1be3b5fb3d42eb98db36
Iganza-roy <iganzaroy55@gmail.com>
2024-08-29 23:39:09 +0300

    Add task 1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-def is_safe(board, row, col):
-    """Checks if placing a queen at (row, col) is safe."""
+import sys
+
+def is_safe(board, row, col, N):
     for i in range(row):
         if board[i] == col or \
-           abs(board[i] - i) == abs(col - row):
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
             return False
     return True
 
 def solve_nqueens(N):
-    """Solves the N-Queens problem."""
     def backtrack(row, board):
         if row == N:
-            print(board)
+            print([[i, board[i]] for i in range(N)])
         else:
             for col in range(N):
-                if is_safe(board, row, col):
+                if is_safe(board, row, col, N):
                     board[row] = col
                     backtrack(row + 1, board)
-                    board[row] = -1
 
     board = [-1] * N
     backtrack(0, board)
 
 if __name__ == "__main__":
-    N = int(input("Enter the board size (N): "))
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
     if N < 4:
         print("N must be at least 4")
-    else:
-        solve_nqueens(N)
+        sys.exit(1)
+    
+    solve_nqueens(N)
