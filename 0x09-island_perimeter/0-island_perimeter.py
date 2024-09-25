e11d0e95da210b1afcc6de3d4cb42eb2ab099e8b
Iganza-roy <iganzaroy55@gmail.com>
2024-09-25 21:45:51 +0300

    Add 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Island Perimeter 
+"""
+
+def island_perimeter(grid):
+    """
+     returns the perimeter of the island described in grid
+     Args:
+        grid: a 2D list of 0s and 1s representing a grid of islands
+    """
