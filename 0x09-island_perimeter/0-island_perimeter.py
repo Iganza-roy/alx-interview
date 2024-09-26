8de63b7bbf3150595b3c1e6a2de26432b5a37db2
Iganza-roy <iganzaroy55@gmail.com>
2024-09-26 22:49:08 +0300

    Refactor island_perimeter function to improve readability and add detailed documentation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-"""
-Island Perimeter 
-"""
+
+"creating a funtiong returns the perimeter of the island described in grid"
+
 
 def island_perimeter(grid):
     """
-     returns the perimeter of the island described in grid
-     Args:
-        grid: a 2D list of 0s and 1s representing a grid of islands
+    defining the island_perimeter funtion that \ 
+    returns the perimeter of the island described in grid:
+
+    Args:
+    grid (list): 0 represents a water zone
+                1 represents a land zone
+                One cell is a square with side length 1
+                Grid cells are connected horizontally/vertically (not diagonally).
+                Grid is rectangular, width and height don’t exceed 100
     """
+    perim = 0
+
+    for i in range(len(grid)):
+        for k in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perim += 4
+                if (i > 0 and grid[i - 1][k] == 1):
+                    perim -= 2
+                if (K > 0 and grid[i][k - 1] == 1):
+                    perim -= 2
+    return perim
+            
+            
+
+
