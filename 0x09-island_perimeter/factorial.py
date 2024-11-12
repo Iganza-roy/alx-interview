22c2e21382cf18b18cd144bd0a981b7e6fcec4d4
Iganza-roy <iganzaroy55@gmail.com>
2024-11-12 21:20:01 +0300

    Add factorial function with input validation and recursive implementation

---
+++ 0x09-island_perimeter/factorial.py
@@ -0,0 +1,8 @@
+def factorial(num):
+    assert num >= 0 and int(num) == num, "num must be ..."
+    if num in [0, 1]:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+print(factorial(4.5))  
